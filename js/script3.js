81089ebe66dffe686bd8dd95494111f8c1c20ec2
QIUYUEN <140587861+YUNEQIU@users.noreply.github.com>
2025-03-25 09:40:51 +0800

    Update script3.js

--- js/script3.js
+++ js/script3.js
@@ -3,7 +3,3 @@ window.addEventListener('scroll', () => {
     const scrollOffset = window.scrollY;
     hero.style.backgroundPositionY = `${scrollOffset * 0.7}px`;
 });
-
-if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-    window.location.href = "https://shedusw.com/mobile.html"; 
-}
