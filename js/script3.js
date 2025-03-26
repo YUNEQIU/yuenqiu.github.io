2ecf478d369423be6b40db99e1f1bb0a1e89fc04
QIUYUEN <140587861+YUNEQIU@users.noreply.github.com>
2025-03-26 12:05:50 +0800

    Update script3.js

--- js/script3.js
+++ js/script3.js
@@ -1,7 +1,7 @@
 const hero = document.getElementById('mascot');
 window.addEventListener('scroll', () => {
     const scrollOffset = window.scrollY;
-    hero.style.backgroundPositionY = `${scrollOffset * 0.7}px`;
+    hero.style.backgroundPositionY = `${scrollOffset * 0.3}px`;
 });
 
 if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
