e1228d4027d8eb2c2d6956114887ecd85794dd34
QIUYUEN <140587861+YUNEQIU@users.noreply.github.com>
2025-04-19 12:39:47 +0800

    Add files via upload

---
+++ js/scripts4.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll('.image');
+  
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        entry.target.style.transitionDelay = `${index * 0.4}s`; // 每张图片的延迟递增
+      }
+    });
+  }, {
+    threshold: 0.5
+  });
+  
+  images.forEach(image => {
+    observer.observe(image);
+  });
+});
+
+const image = document.getElementById('myImage');
+let hasAnimated = false; 
+
+function checkIfInView() {
+  const rect = image.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+  if (!hasAnimated && rect.top <= windowHeight && rect.bottom >= 0) {
+    hasAnimated = true; 
+    setTimeout(() => {
+      image.classList.add('show');
+    }, 1500); 
+  }
+}
+
+window.addEventListener('scroll', checkIfInView);
+window.addEventListener('load', checkIfInView);
