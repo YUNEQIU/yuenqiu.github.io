bdfc00efd7f1e1614861513a1d8aed971cc92f00
QIUYUEN <140587861+YUNEQIU@users.noreply.github.com>
2025-06-10 16:31:40 +0800

    Update script.js

--- artteam/js/script.js
+++ artteam/js/script.js
@@ -84,3 +84,13 @@ document
       searchSoftware();
     }
   });
+
+// logo页面
+document.addEventListener("DOMContentLoaded", () => {
+  const wrappers = document.querySelectorAll(".image-wrapper");
+  wrappers.forEach((wrapper, index) => {
+    setTimeout(() => {
+      wrapper.classList.add("fade-in");
+    }, index * 200); // 每个图片相隔200ms依次出现
+  });
+});
