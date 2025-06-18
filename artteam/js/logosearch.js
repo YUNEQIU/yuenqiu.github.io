eb8bb67c9c0b5bea6cc53c33f2df9f8ce7b2d6e3
QIUYUEN <140587861+YUNEQIU@users.noreply.github.com>
2025-06-18 12:50:50 +0800

    Update logosearch.js

--- artteam/js/logosearch.js
+++ artteam/js/logosearch.js
@@ -2,17 +2,282 @@ const softwareData = [
 
   {
     name: "上海开放大学LOGO",
-    file: "test1.psd",
+    file: "上海开放大学LOGO.ai",
     description: "上海开放大学LOGO",
     page: "logo1.html",
     thumbnail: "images/logo/logo1/logo_2.png",
   },
+
   {
     name: "上海学习网LOGO",
-    file: "test1.psd",
+    file: "上海开放大学LOGO.ai",
     description: "上海学习网LOGO",
     page: "logo2.html",
     thumbnail: "images/logo/logo2/logo_1.png",
+  },
+
+  {
+    name: "上海政府LOGO",
+    file: "上海市政府logo.ai",
+    description: "上海政府LOGO",
+    page: "logo3.html",
+    thumbnail: "images/logo/logo3/logo_1.png",
+  },
+
+  {
+    name: "中德开放在线学习平台LOGO",
+    file: "中德开放在线学习平台logo.ai",
+    description: "中德开放在线学习平台LOGO",
+    page: "logo4.html",
+    thumbnail: "images/logo/logo4/logo_1.png",
+  },
+
+  {
+    name: "上海市终身教育学分银行LOGO",
+    file: "上海市终身教育学分银行logo.ai",
+    description: "上海市终身教育学分银行LOGO",
+    page: "logo5.html",
+    thumbnail: "images/logo/logo5/logo_1.png",
+  },
+
+  {
+    name: "上海教育软件发展有限公司LOGO",
+    file: "上海教育软件发展有限公司logo.ai",
+    description: "上海教育软件发展有限公司LOGO",
+    page: "logo6.html",
+    thumbnail: "images/logo/logo6/logo_1.png",
+  },
+
+  {
+    name: "上海教育软件发展有限公司旧版LOGO",
+    file: "上海教育软件发展有限公司旧版LOGO.ai",
+    description: "上海教育软件发展有限公司旧版LOGO",
+    page: "logo7.html",
+    thumbnail: "images/logo/logo7/logo_1.png",
+  },
+
+  {
+    name: "市民终身学习周LOGO",
+    file: "市民终身学习周logo.ai",
+    description: "市民终身学习周LOGO",
+    page: "logo8.html",
+    thumbnail: "images/logo/logo8/logo_1.png",
+  },
+
+  {
+    name: "银发e学堂LOGO",
+    file: "银发e学堂logo.ai",
+    description: "银发e学堂LOGO",
+    page: "logo9.html",
+    thumbnail: "images/logo/logo9/logo_1.png",
+  },
+
+  {
+    name: "民生学院LOGO",
+    file: "民生学院logo.ai",
+    description: "民生学院LOGO",
+    page: "logo10.html",
+    thumbnail: "images/logo/logo10/logo_1.png",
+  },
+
+  {
+    name: "上海老年教育慕课LOGO",
+    file: "上海老年教育慕课logo.ai",
+    description: "上海老年教育慕课LOGO",
+    page: "logo11.html",
+    thumbnail: "images/logo/logo11/logo_1.png",
+  },
+
+  {
+    name: "上海微校LOGO",
+    file: "上海微校logo.ai",
+    description: "上海微校LOGO",
+    page: "logo12.html",
+    thumbnail: "images/logo/logo12/logo_1.png",
+  },
+
+  {
+    name: "黄浦老年教育LOGO",
+    file: "黄浦老年教育logo.ai",
+    description: "黄浦老年教育LOGO",
+    page: "logo13.html",
+    thumbnail: "images/logo/logo13/logo_1.png",
+  },
+
+  {
+    name: "上海市民终身学习体验基地LOGO",
+    file: "上海市民终身学习体验基地logo.ai",
+    description: "上海市民终身学习体验基地LOGO",
+    page: "logo14.html",
+    thumbnail: "images/logo/logo14/logo_1.png",
+  },
+
+  {
+    name: "上海市教师教育学院LOGO",
+    file: "上海市教师教育学院logo.ai",
+    description: "上海市教师教育学院LOGO",
+    page: "logo15.html",
+    thumbnail: "images/logo/logo15/logo_2.png",
+  },
+
+  {
+    name: "上海市学习型社会建设服务指导中心LOGO",
+    file: "上海市学习型社会建设服务指导中心logo.ai",
+    description: "上海市学习型社会建设服务指导中心LOGO",
+    page: "logo16.html",
+    thumbnail: "images/logo/logo16/logo_1.png",
+  },
+
+  {
+    name: "学在黄浦LOGO",
+    file: "学在黄浦logo.ai",
+    description: "学在黄浦LOGO",
+    page: "logo17.html",
+    thumbnail: "images/logo/logo17/logo_1.png",
+  },
+
+  {
+    name: "闵行学习LOGO",
+    file: "闵行学习logo.ai",
+    description: "闵行学习LOGO",
+    page: "logo18.html",
+    thumbnail: "images/logo/logo18/logo_1.png",
+  },
+
+  {
+    name: "普陀学习LOGO",
+    file: "普陀学习logo.ai",
+    description: "普陀学习LOGOO",
+    page: "logo19.html",
+    thumbnail: "images/logo/logo19/logo_1.png",
+  },
+
+  {
+    name: "长三角职业教育一体化协同发展联盟LOGO",
+    file: "长三角职业教育一体化协同发展联盟logo.ai",
+    description: "长三角职业教育一体化协同发展联盟LOGO",
+    page: "logo20.html",
+    thumbnail: "images/logo/logo20/logo_1.png",
+  },
+
+  {
+    name: "2024闵行区市民读书活动LOGO",
+    file: "2024闵行区市民读书活动logo.ai",
+    description: "2024闵行区市民读书活动LOGO",
+    page: "logo21.html",
+    thumbnail: "images/logo/logo21/logo_1.png",
+  },
+
+  {
+    name: "汇地图LOGO",
+    file: "汇地图logo.ai",
+    description: "汇地图LOGO",
+    page: "logo22.html",
+    thumbnail: "images/logo/logo22/logo_1.png",
+  },
+
+  {
+    name: "社区教育云视课堂LOGO",
+    file: "社区教育云视课堂logo.ai",
+    description: "社区教育云视课堂LOGO",
+    page: "logo23.html",
+    thumbnail: "images/logo/logo23/logo_1.png",
+  },
+
+  {
+    name: "上海教育资源中心LOGO",
+    file: "上海教育资源中心logo.ai",
+    description: "上海教育资源中心LOGO",
+    page: "logo24.html",
+    thumbnail: "images/logo/logo24/logo_1.png",
+  },
+
+  {
+    name: "学习，遇到更好的自己LOGO",
+    file: "学习遇到更好的自己logo.psd",
+    description: "学习，遇到更好的自己LOGO",
+    page: "logo25.html",
+    thumbnail: "images/logo/logo25/logo_1.png",
+  },
+
+  {
+    name: "巾帼大学堂LOGO",
+    file: "logo_1.png",
+    description: "巾帼大学堂LOGO",
+    page: "logo26.html",
+    thumbnail: "images/logo/logo26/logo_1.png",
+  },
+
+  {
+    name: "学习坊LOGO",
+    file: "学习坊logo.ai",
+    description: "学习坊LOGO",
+    page: "logo27.html",
+    thumbnail: "images/logo/logo27/logo_1.png",
+  },
+
+  {
+    name: "多语言在线学习平台LOGO学习坊LOGO",
+    file: "多语言在线学习平台logo.ai",
+    description: "多语言在线学习平台LOGO",
+    page: "logo28.html",
+    thumbnail: "images/logo/logo28/logo_1.png",
+  },
+
+  {
+    name: "上海市电视中等专业学校LOGO",
+    file: "上海市电视中等专业学校logo.ai",
+    description: "上海市电视中等专业学校LOGO",
+    page: "logo29.html",
+    thumbnail: "images/logo/logo29/logo_1.png",
+  },
+
+  {
+    name: "上海电达信息技术有限公司LOGO",
+    file: "上海电达信息技术有限公司logo.ai",
+    description: "上海电达信息技术有限公司LOGO",
+    page: "logo30.html",
+    thumbnail: "images/logo/logo30/logo_1.png",
+  },
+
+  {
+    name: "上海市中小学专题教育LOGO",
+    file: "上海市中小学专题教育logo.ai",
+    description: "上海市中小学专题教育LOGO",
+    page: "logo31.html",
+    thumbnail: "images/logo/logo31/logo_1.png",
+  },
+
+  {
+    name: "上海市学生综合素质评价信息管理系统LOGO",
+    file: "上海学生综评logo.ai",
+    description: "上海市学生综合素质评价信息管理系统LOGO",
+    page: "logo32.html",
+    thumbnail: "images/logo/logo32/logo_1.png",
+  },
+
+  {
+    name: "上海教育电视台LOGO",
+    file: "上海教育电视台logo.ai",
+    description: "上海教育电视台LOGO",
+    page: "logo33.html",
+    thumbnail: "images/logo/logo33/logo_1.png",
+  },
+
+  {
+    name: "上海市教师教育网LOGO",
+    file: "上海市教师教育网logo.ai",
+    description: "上海市教师教育网LOGO",
+    page: "logo34.html",
+    thumbnail: "images/logo/logo34/logo_1.png",
+  },
+
+  {
+    name: "上海市教育委员会教育技术装备中心LOGO",
+    file: "上海市教育委员会教育技术装备中心logo.ai",
+    description: "上海市教育委员会教育技术装备中心LOGO",
+    page: "logo35.html",
+    thumbnail: "images/logo/logo35/logo_1.png",
   },
 
 ];
